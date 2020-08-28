9194d2a7ce727708f8cb6ac2a92800bc1f108c9a
Prasanjit Sarkar <38756751+Prasanjit137@users.noreply.github.com>
2020-08-28 09:28:29 -1200

    Delete Binary Equivalent.py

--- Binary Equivalent.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    num = int(input("Enter the number : "))
-    a = []
-    a = list(map(int, input().split(' ')))
-    for i in range(0, len(a)):
-        a[i] = bin(int(a[i]))[2:]
-    print(a)
